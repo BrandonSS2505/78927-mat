0e8a175dcdb8417ec5c2579dcd9fe948e7432958
BrandonSS2505 <105601441+BrandonSS2505@users.noreply.github.com>
2023-02-28 11:26:34 -0600

    commit clase

--- Practica02/Services/Operaciones.cs
+++ Practica02/Services/Operaciones.cs
@@ -1,19 +1,35 @@
 using System;
 using WSDL.mensajes;
-//va a tener su propio namespace para poder luego importarla
-namespace WSDL.operaciones 
-{
+//va a tener su propio namespace para poder importarla despues
+namespace WSDL.operaciones{
     //esta si es una clase
-    public class Operaciones : Mensajes
-    {
-        public string Saludar(string nombre)
-        {
-            string msj = "hola " +nombre;
-            return msj;
+    public class Operaciones : Mensajes{
+        private string[] mensajes = new string[2];
+ 
+        public string Saludar(string nombre){
+            Boolean estaLleno = true;
+            for(int x = 0; x < mensajes.Length; x++){
+                if(string.IsNullOrEmpty(mensajes[x])){
+                    mensajes[x]=nombre;
+                    estaLleno = false;
+                    break;
+                }
+            }
+
+            if (estaLleno == true){
+                return "Memoria llena..";
+            }
+            return "Hola "+nombre;
         }
-        public string Mostrar(int id)
-        {
-            return "x";
+        public string Mostrar(int id){
+            if(id >= mensajes.Length || id < 0){
+                return "No existe este id";
+            } 
+            
+            if(!string.IsNullOrEmpty(mensajes[id])){
+                return "Hola y bienvenido " + mensajes[id];
+            }
+            return "No se encontro el mensaje con el id";
         }
     }
 }
